1ea23d3cc1cfa897ad63e5add856ee4b6b4110ad
BCorr17 <CorrBradley@gmail.com>
2014-03-25 10:02:17 -0400

    srgrgfgerg

---
+++ Chapter4Problems/Problem1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            string input;
+            int n;
+            while (true)
+            {
+                Console.Write("input number here:");
+                input = Console.ReadLine();
+                n = Convert.ToInt32(input);
+                if (input == "999")
+                {
+                    break;
+                }
+                if (n < 999)
+                {
+                    sum += n;
+                }
+                Console.WriteLine("Sum is {0}", sum);
+
+                 
+            }
+        }
+    }
+}
